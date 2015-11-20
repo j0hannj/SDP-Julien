2efd3ac47815619a5ea982917a4e5b97298d9845
johann <johann.julien@gmail.com>
2015-11-20 10:47:51 +0100

    money

---
+++ src/main/java/change/Money.java
@@ -0,0 +1,25 @@
+package change;
+
+/**
+ * Created by johann on 20/11/2015.
+ */
+public class Money {
+
+    /**
+     * Created by johann on 20/11/2015.
+     */
+        String name;
+        double usd;
+
+        public Money(String name, double usd) {
+            this.name = name;
+            this.usd = usd;
+        }
+
+        public double getChange() {
+            return this.usd;
+        }
+
+
+    }
+
