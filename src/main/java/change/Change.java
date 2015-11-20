8c54f0a55e9986ce08d013865a607ffc75a523be
johann <johann.julien@gmail.com>
2015-11-20 10:56:14 +0100

    Change

---
+++ src/main/java/change/Change.java
@@ -0,0 +1,17 @@
+package change;
+import javax.swing.JFrame;
+
+/**
+ * Created by johann on 20/11/2015.
+ */
+public class Change {
+    public static void main(String[] args)
+    {
+        JFrame fenetre= new JFrame();
+
+        fenetre.setTitle("taux de change");
+        fenetre.setSize(400,400);
+        fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        fenetre.setVisible(true);
+    }
+}
